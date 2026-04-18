58448ff9bac61bfdce09f840aed8957028c5a59f
Dan Artillaga <artillagadan@gmail.com>
2026-04-18 08:43:13 +0800

    add index page for easi api testing

--- app/web/app.py
+++ app/web/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from web.routes import scan
 from web.routes import health
 
@@ -7,3 +8,5 @@ app = FastAPI(title="Edge Tile Inspection Robot API")
 # include API routers
 app.include_router(health.router)
 app.include_router(scan.router)
+
+app.mount("/", StaticFiles(directory="web/static", html=True), name="static")
