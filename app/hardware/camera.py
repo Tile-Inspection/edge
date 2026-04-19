02e98950b92f2a5cb68f06bc3c51807682e3425b
Dan Artillaga <artillagadan@gmail.com>
2026-04-19 15:10:46 +0800

    fix camera capture error

--- app/hardware/camera.py
+++ app/hardware/camera.py
@@ -3,15 +3,31 @@ try:
     PICAM_AVAILABLE = True
 except ImportError:
     PICAM_AVAILABLE = False
+    
+import time
 
 class Camera:
+    def __init__(self):
+        self.picam2 = None
+        if PICAM_AVAILABLE:
+            self.picam2 = Picamera2()
+            self.picam2.start()
+            time.sleep(2)
+            print("Camera initialized successfully.")
+        else:
+            print("Camera not available.")
+    
     def capture(self):
         if PICAM_AVAILABLE:
             filename = "image.jpg"
-            picam2 = Picamera2()
-            picam2.start()
-            picam2.capture_file(filename)
-            picam2.stop()
+            self.picam2.capture_file(filename)
             return filename
+        else:
+            print("Picamera2 not available")
+            
+    def close(self):
+        if PICAM_AVAILABLE:
+            self.picam2.stop()
+            print("Camera closed.")
         else:
             print("Picamera2 not available")
--- app/main.py
+++ app/main.py
@@ -29,4 +29,5 @@ if __name__ == "__main__":
         print("Shutting down...")
     finally:
         controller.serial_communicator.close()
+        controller.camera.close()
     
