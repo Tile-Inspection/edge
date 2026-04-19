77e0d02313171a2c820304376697fd067bc778e6
Dan Artillaga <artillagadan@gmail.com>
2026-04-19 14:06:32 +0800

    add camera integration

--- app/hardware/camera.py
+++ app/hardware/camera.py
@@ -1,4 +1,17 @@
+try:
+    from picamera2 import Picamera2
+    PICAM_AVAILABLE = True
+except ImportError:
+    PICAM_AVAILABLE = False
+
 class Camera:
     def capture(self):
-        print("Capturing image")
-        return "image.jpg"
+        if PICAM_AVAILABLE:
+            filename = "image.jpg"
+            picam2 = Picamera2()
+            picam2.start()
+            picam2.capture_file(filename)
+            picam2.stop()
+            return filename
+        else:
+            print("Picamera2 not available")
