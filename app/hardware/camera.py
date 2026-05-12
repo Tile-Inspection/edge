99bc609af928d980c5b29dc28c2425c14c76c62d
Dan Artillaga <artillagadan@gmail.com>
2026-05-12 10:50:57 +0800

    initialize camera with config

--- app/hardware/camera.py
+++ app/hardware/camera.py
@@ -12,6 +12,8 @@ class Camera:
         try:
             if PICAM_AVAILABLE:
                 self.picam2 = Picamera2()
+                config = self.picam2.create_still_configuration()
+                self.picam2.configure(config)
                 self.picam2.start()
                 time.sleep(2)
                 print("Camera initialized successfully.")
