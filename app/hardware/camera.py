ca459b79cb11a700714946ebe3d1256051190564
Dan Artillaga <artillagadan@gmail.com>
2026-05-16 12:35:23 +0800

    set camera configuration to use lower resolution

--- app/hardware/camera.py
+++ app/hardware/camera.py
@@ -7,13 +7,15 @@ except ImportError:
 import time
 import cv2
 
+from constants import X_DIM, Y_DIM
+
 class Camera:
     def __init__(self):
         self.picam2 = None
         try:
             if PICAM_AVAILABLE:
                 self.picam2 = Picamera2()
-                config = self.picam2.create_still_configuration()
+                config = self.picam2.create_still_configuration(main={"size": (X_DIM, Y_DIM)})
                 self.picam2.configure(config)
                 self.picam2.start()
                 time.sleep(2)
