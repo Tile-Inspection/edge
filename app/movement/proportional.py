85daf54c4ad821d2b584f6485f7d88423469532f
Dan Artillaga <artillagadan@gmail.com>
2026-05-19 02:20:34 +0800

    update proportional controller

--- app/movement/proportional.py
+++ app/movement/proportional.py
@@ -1,5 +1,5 @@
 class Proportional:
-    def __init__(self, kp=0.0005):
+    def __init__(self, kp=0.1):
         self.kp = kp
         self.prev_error = 0
         self.integral = 0
@@ -7,7 +7,7 @@ class Proportional:
     def compute(self, error):
         angular_velocity = self.kp * error
 
-        angular_velocity = max(-0.5, min(0.5, angular_velocity))
+        angular_velocity = max(-1, min(1, angular_velocity))
         
         self.prev_error = error
 
