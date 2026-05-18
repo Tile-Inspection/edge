b444a725d59768f39e20907d875fd0f01ae69e3a
Dan Artillaga <artillagadan@gmail.com>
2026-05-19 02:18:02 +0800

    refactor send_velocity method to include value scaling

--- app/hardware/motion.py
+++ app/hardware/motion.py
@@ -12,6 +12,11 @@ class Motion:
     
     def send_velocity(self, linear: float, angular: float):
         """Updates the current command using the new velocity protocol."""
+        
+        # Scale values to be between -0.3 and 0.3
+        linear = max(-0.3, min(0.3, linear))
+        angular = max(-0.3, min(0.3, angular))
+        
         self.serial.set_command(self._format_velocity_command(linear, angular))
         
     def stop(self):
